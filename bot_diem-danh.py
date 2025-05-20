681fbdf9e444bf7a35485259018dc7a0f390984a
HoagAh <141402080+HoagAh@users.noreply.github.com>
2025-05-20 23:36:57 +0700

    Update bot_diem-danh.py

--- bot_diem-danh.py
+++ bot_diem-danh.py
@@ -5,6 +5,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from datetime import datetime
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+PASSWORD = "28122025"
+AUTHORIZED_USERS = set()
 attendance_list = []
 
 tz_vietnam = pytz.timezone('Asia/Ho_Chi_Minh')
@@ -19,6 +21,14 @@ async def hi(update: Update, context: ContextTypes.DEFAULT_TYPE):
     time = now.strftime('%H:%M:%S %d/%m/%Y')
     attendance_list.append((user, time))
     await update.message.reply_text(f"{user} đã điểm danh lúc {time}")
+
+async def handle_password(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    if update.message.text == PASSWORD:
+        AUTHORIZED_USERS.add(user_id)
+        await update.message.reply_text("✅ Xác thực thành công! Bây giờ bạn có thể dùng lệnh /timfile <từ_khóa>")
+    else:
+        await update.message.reply_text("❌ Mật khẩu sai!")
 
 app = ApplicationBuilder().token("7949088784:AAG0rkhlmIVz_kn1EDreWaFB2Pd6iyoBQJU").build()
 app.add_handler(CommandHandler("start", start))
