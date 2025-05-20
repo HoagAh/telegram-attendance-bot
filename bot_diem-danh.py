451985692260aa67232e93637cea52c459747bc5
HoagAh <141402080+HoagAh@users.noreply.github.com>
2025-05-20 22:57:51 +0700

    Update bot_diem-danh.py

--- bot_diem-danh.py
+++ bot_diem-danh.py
@@ -1,4 +1,5 @@
 import os
+import pytz
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from datetime import datetime
@@ -6,13 +7,16 @@ from datetime import datetime
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 attendance_list = []
 
+tz_vietnam = pytz.timezone('Asia/Ho_Chi_Minh')
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Chào mừng! Gõ /hi để điểm danh.")
 
 async def hi(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user.full_name
-    time = datetime.now().strftime('%H:%M:%S %d/%m/%Y')
+    now = datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
+    time = now.strftime('%H:%M:%S %d/%m/%Y')
     attendance_list.append((user, time))
     await update.message.reply_text(f"{user} đã điểm danh lúc {time}")
 
